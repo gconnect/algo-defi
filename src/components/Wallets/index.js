ee7bed102bc1483a7246c8549c8c158e50953081
Glory Agatevure <agatevureglory@gmail.com>
2022-02-10 23:38:06 +0100

    Implemented algosigner

--- src/components/Wallets/index.js
+++ src/components/Wallets/index.js
@@ -1,14 +1,38 @@
+/*global AlgoSigner*/
 import logo1 from '../../assets/img/myalgo.png'
 import logo2 from '../../assets/img/algo.png'
+import algosigner from '../../assets/img/algosigner'
 import { Wrapper, Logo, Button } from './Wallets.styles'
 
 const Wallets = () => {
+  const [showAccount, setShowAccount]= useState(false)
+  const userAccount = useRef()
+
+  //Connect Algosigner
+  const connectAlgoSigner = async () =>{
+    let resp = await AlgoSigner.connect()
+    console.log(resp)    
+    getUserAccount()
+    setShowAccount(true)
+  }
+
+  const getUserAccount = async () =>{
+     userAccount.current =  await AlgoSigner.accounts({
+          ledger: 'TestNet'
+        })
+        if(userAccount.current[0]['address'] !== null){
+          setShowAccount(true)
+        }
+  console.log(userAccount.current[0]['address'])
+  // console.log(userAccount.current)
+}
+
   return (
     <Wrapper>
       <h2>Connect to a Wallet</h2>
+      <Button onClick={connectAlgoSigner}><Logo src={algosigner} /> Algo Signer</Button>
       <Button><Logo src={logo1} /> My Algo Wallet</Button>
       <Button><Logo src={logo2} /> Algorand Wallet</Button>
-      <Button><Logo src={logo2} /> Algo Signer</Button>
     </Wrapper>
 )};
 
