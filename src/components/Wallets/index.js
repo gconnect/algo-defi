0489149eeb94963a7c8795fb2eff0e1385918011
Glory Agatevure <agatevureglory@gmail.com>
2022-02-10 23:54:30 +0100

    commented unused files

--- src/components/Wallets/index.js
+++ src/components/Wallets/index.js
@@ -14,7 +14,7 @@ const Wallets = () => {
     let resp = await AlgoSigner.connect()
     console.log(resp)    
     getUserAccount()
-    setShowAccount(true)
+    // setShowAccount(true)
   }
 
   const getUserAccount = async () =>{
@@ -22,7 +22,7 @@ const Wallets = () => {
           ledger: 'TestNet'
         })
         if(userAccount.current[0]['address'] !== null){
-          setShowAccount(true)
+          // setShowAccount(true)
         }
   console.log(userAccount.current[0]['address'])
   // console.log(userAccount.current)
